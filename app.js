ac6d1d49bd70ef514c5c75b84a766183f96280f5
SAGUN JAISWAL <63302439+sagunjaiswal@users.noreply.github.com>
2020-09-29 01:59:41 +0530

    mongodb atlas setup

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const dbURI = process.env.mongoURI;
+mongoose.connect();
 //express app
 const app = express();
 //register view engine
 app.set("view engine", "ejs");
 app.listen(3000);
+
+app.use(express.static("public"));
+app.use(morgan("dev"));
+
 app.get("/", (req, res) => {
   //   res.sendFile("./files/index.html", { root: __dirname });
   const blogs = [
@@ -13,6 +21,7 @@ app.get("/", (req, res) => {
   ];
   res.render("index", { title: "Home", blogs: blogs });
 });
+
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
